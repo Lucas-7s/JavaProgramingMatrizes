f7b0c0e679c0290e10dcb139c0d46e82e3864058
Lucas S Silva <143051956+Lucas-7s@users.noreply.github.com>
2025-07-23 03:01:10 -0300

    Update Ex01Matriz3x3.java

--- src/com/lucas/javatraining/Ex01Matriz3x3.java
+++ src/com/lucas/javatraining/Ex01Matriz3x3.java
@@ -1,40 +1,43 @@
 /* Exercicio
+ * Criar e Imprimir uma Matriz
  * Crie uma matriz 3x3 de inteiros e preencha com números de 1 a 9. Imprima a matriz.
  */
 
 package com.lucas.javatraining;
 
-import java.util.Random;
-
 public class Ex01Matriz3x3 {
 	public static void main(String[] args) {
 		
-		String titulo = "Matriz 3x3 com números aleatórios";
-		System.out.println("=".repeat(55) + "\n");
-		System.out.println(" ".repeat((55 - titulo.length()) / 2) + titulo + "\n");
+		//intro
+		String titulo = "Matriz 3x3 com números de 1 à 9";
+		System.out.println("=".repeat(55));
+		System.out.println(" ".repeat((55 - titulo.length()) / 2) + titulo);
 		System.out.println("=".repeat(55) + "\n");
 		
-		Random r = new Random();
+		//inicia a matriz
 		int [][] matriz = new int [3][3];
-		for (int l = 0; l < matriz.length; l++) {
-			for (int c = 0; c < matriz[l].length; c++) {
-				matriz[l][c] = (int)(Math.random()*10);
-				System.out.print("Posição " +l + "," + c + ": " + matriz[l][c]);
+		
+		//popula matriz com soma
+		int contador = 0;
+		for (int linha = 0; linha < matriz.length; linha++) {
+			for (int coluna = 0; coluna < matriz[linha].length; coluna++) {
+				contador++;
+				matriz[linha][coluna] = contador;
+				System.out.print(matriz[linha][coluna] + " | ");
 			}
 			System.out.println("");
 		}
-
+		
 		System.out.println("");
 		
+		//reexibe a matriz com sua posição
 		for (int l = 0; l < matriz.length; l++) {
 			for (int c = 0; c < matriz[l].length; c++) {
-				System.out.print(matriz[l][c] + " ");
+				System.out.print("Posição " +l + "," + c + ": " + matriz[l][c] + " | ");
 			}
 			System.out.println("");
 		}
-		
 		System.out.println("\n" + "=".repeat(55));
-
 	}
 
 }
