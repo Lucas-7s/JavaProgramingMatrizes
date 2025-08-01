5b9a2ec11c75dda65df4011c37eb6981f1fb1d28
Lucas S Silva <143051956+Lucas-7s@users.noreply.github.com>
2025-08-01 18:47:06 -0300

    Create Ex08MatrizIdentidade.java

---
+++ src/com/lucas/javatraining/Ex08MatrizIdentidade.java
@@ -0,0 +1,43 @@
+/* Exercicio
+ * Matriz Identidade
+ * Crie um método que gera uma matriz identidade de ordem n (n x n).
+ */
+
+package com.lucas.javatraining;
+
+import java.util.Random;
+
+public class Ex08MatrizIdentidade {
+
+	public static void main(String[] args) {
+		
+		Random r = new Random();
+		
+		//declaração de variáveis
+		String titulo = "Programa Que Faz Matriz Identidade de ordem n x n";
+		int numMatriz = (int)(Math.random()*5)+2;
+		
+		int matrizIdentidade[][] = new int [numMatriz][numMatriz];
+		
+		//intro
+		System.out.println("=".repeat(50));
+		System.out.println(" ".repeat((50 - titulo.length())/2) + titulo.toUpperCase());
+		System.out.println("=".repeat(50) + "\n");
+		
+		System.out.println("Matriz " + matrizIdentidade.length + "x" + matrizIdentidade[0].length + ":");
+		for(int l = 0; l < matrizIdentidade.length; l++) {
+			for(int c = 0; c < matrizIdentidade[l].length; c++) {
+				
+				if(matrizIdentidade[l] == matrizIdentidade[c]) {
+					matrizIdentidade[l][c] = 1;
+				}
+				System.out.print(matrizIdentidade[l][c] + " | ");
+			}
+			System.out.println("");
+		}
+		
+
+		System.out.println("\n" + "=".repeat(50));
+	}
+
+}
