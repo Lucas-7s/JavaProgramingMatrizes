9968067d8b80c5d5edf80d5d4345eafa968a173d
Lucas S Silva <143051956+Lucas-7s@users.noreply.github.com>
2025-10-03 13:09:28 -0300

    Update

--- src/com/lucas/javatraining/Ex18MatrizSimetria.java
+++ src/com/lucas/javatraining/Ex18MatrizSimetria.java
@@ -9,6 +9,17 @@ package com.lucas.javatraining;
 public class Ex18MatrizSimetria {
 	public static void main(String[] args) {
 		
+		//declaracao de variaveis
+		String titulo = "Verificar Simetria";
+		
+		//intro
+		System.out.println("=".repeat(55));
+		System.out.println(" ".repeat((55 - titulo.length())/2) + titulo.toUpperCase());
+		System.out.println("=".repeat(55) + "\n");
+		
+		System.out.println("\n" + "=".repeat(55));
+		
+		
 	}
 
 }
