76a133fabc2d8fad19009f1d0bb5d3228cf93b27
Lucas S Silva <143051956+Lucas-7s@users.noreply.github.com>
2025-10-03 12:43:19 -0300

    Create Ex18MatrizSimetria.java

---
+++ src/com/lucas/javatraining/Ex18MatrizSimetria.java
@@ -0,0 +1,14 @@
+/* Exercicio
+ * Verificar Simetria
+ * Crie um método que verifica se uma matriz é simétrica (a[i][j] == a[j][i]).
+ */
+
+
+package com.lucas.javatraining;
+
+public class Ex18MatrizSimetria {
+	public static void main(String[] args) {
+		
+	}
+
+}
